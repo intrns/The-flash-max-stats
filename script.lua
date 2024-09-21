922fbb6a7280d6ad4f6eed54025f6b6eb757b6fa
intrns <iamalightmain@gmail.com>
2024-09-21 17:31:49 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-// this script is for https://www.roblox.com/games/13796645198/2X-FLASHPOINT-Worlds-Collide-BETA 
+-- this script is for https://www.roblox.com/games/13796645198/2X-FLASHPOINT-Worlds-Collide-BETA 
 
 game:GetService("Players").LocalPlayer.PlayerData.Upgrades["Boost Duration"].Value = 50
 game:GetService("Players").LocalPlayer.PlayerData.Upgrades.Damage.Value = 50
